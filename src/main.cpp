ef93c83107baaedbf52d733c0bd3c846bf8dee19
Leph <leph.quentinrouxel@gmail.com>
2013-01-30 12:44:01 +0100

    Cleanning up

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,24 @@
 int main()
 {
     try {
+        //Load data
         SensorData sensors = SensorData();
         std::cout << "loading..." << std::endl;
         CSVParser::parseFile("../data.csv", sensors);
+
+        //Find walking phases
         std::vector< std::pair<size_t,size_t> > phases = 
             Fitness::findWalkPhases(sensors);
-       for (size_t i=0;i<phases.size();i++) {
+        std::cout << "Walking phases:" << std::endl;
+        for (size_t i=0;i<phases.size();i++) {
             std::cout << "start " << phases[i].first 
-           << " stop " << phases[i].second << std::endl;
+            << " stop " << phases[i].second << std::endl;
         }
-       std::vector<double> fitnesses =Fitness::computeFitness(sensors, phases);
-       Plot::add("Fitnesses for each walk phase",fitnesses);
-       Plot::plot();
+
+        //Compute fitness
+        std::vector<double> fitnesses = Fitness::computeFitness(sensors, phases);
+        Plot::add("Fitnesses for each walk phase", fitnesses);
+        Plot::plot();
     } catch (std::string error) {
         std::cout << "ERROR: " << error << std::endl;
         return EXIT_FAILURE;
